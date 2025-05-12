1ad279f78b426c3889c2e0c04929a5f31427fd2e
runcken <runcken@gmail.com>
2025-05-12 19:32:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from transliterate import translit, get_available_language_codes
 from num2words import num2words
-text = """Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once 
-in a lifetime and I guess that this is mine. People have also told me to make these next 
-few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.
 
-More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8..."""
+text = """Ladies and gentlemen, I'm 78 years old and I finally got \
+15 minutes of fame once in a lifetime and I guess that this is mine. \
+People have also told me to make these next few minutes escruciatingly \
+embarrassing and to take vengeance of my enemies. Neither will happen.
+
+More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic \
+Storage for last 40. When I was 8..."""
+
 print(translit(text, 'ru'))
 print()
 print(78, translit(num2words(78, lang='en'), 'ru'), sep=" - ")
